441c6c1238fdcca86fc16ff1c74dff1adea8ff30
VienNguyen24hdev <viennguyendeveloper@gmail.com>
2020-03-05 16:50:18 +0700

    add core js

---
+++ src/polyfills.js
@@ -0,0 +1,11 @@
+// TODO: target browsers that need it
+import "core-js/es6/promise";
+import "core-js/fn/array/find";
+import "core-js/fn/array/flat-map";
+import "core-js/fn/array/from";
+import "core-js/fn/array/includes";
+import "core-js/fn/object/assign";
+import "core-js/fn/object/values";
+import "core-js/fn/string/ends-with";
+import "core-js/fn/string/includes";
+import "core-js/fn/symbol";
